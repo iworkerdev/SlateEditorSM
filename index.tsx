d6ead045fb3046aa09829695514488bf9004a485
Mshindi Creations <114446531+MshindiCreationsInc@users.noreply.github.com>
2022-10-07 19:36:39 +0300

    feat: :sparkles: create generateMongoId function

--- index.tsx
+++ index.tsx
@@ -1,169 +1,206 @@
-import { Box, Stack, useColorModeValue } from '@chakra-ui/react';
-import { Editable, Slate, withReact } from 'slate-react';
-import { Element, Leaf } from './EditorComponent';
-import { Range, Transforms, createEditor } from 'slate';
-import React, { useCallback, useMemo } from 'react';
-import { insertImage, isImageUrl } from './ImageComponent';
+import { Box, Stack, useColorModeValue } from "@chakra-ui/react"
+import { Editable, Slate, withReact } from "slate-react"
+import { Element, Leaf } from "./EditorComponent"
+import { Range, Transforms, createEditor } from "slate"
+import React, { useCallback, useMemo } from "react"
+import { insertImage, isImageUrl } from "./ImageComponent"
 
-import { CustomEditor } from './CustomTypes';
-import ToolBar from './ToolBar';
-import isHotkey from 'is-hotkey';
-import isUrl from 'is-url';
-import serialize from './Serialize';
-import { toggleMark } from './EditorButton';
-import { withHistory } from 'slate-history';
-import { wrapLink } from './LinkComponent';
+import { CustomEditor } from "./CustomTypes"
+import ToolBar from "./ToolBar"
+import isHotkey from "is-hotkey"
+import isUrl from "is-url"
+import serialize from "./Serialize"
+import { toggleMark } from "./EditorButton"
+import { withHistory } from "slate-history"
+import { wrapLink } from "./LinkComponent"
+
+//generate a unique mongo id
+export const generateMongoId = () => {
+  const timestamp = ((new Date().getTime() / 1000) | 0).toString(16)
+  return (
+    timestamp +
+    "xxxxxxxxxxxxxxxx"
+      .replace(/[x]/g, () => ((Math.random() * 16) | 0).toString(16))
+      .toLowerCase()
+  )
+}
 
 type HotKeys = {
-  [key: string]: string;
-};
+  [key: string]: string
+}
 
 const HOTKEYS: HotKeys = {
-  'mod+b': 'bold',
-  'mod+i': 'italic',
-  'mod+u': 'underline',
-  'mod+`': 'code',
-};
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+  "mod+`": "code",
+}
 
 const withInLines = (editor: CustomEditor) => {
-  const { insertData, insertText, isInline } = editor;
+  const { insertData, insertText, isInline } = editor
 
-  editor.isInline = element =>
-    ['link', 'button'].includes(element.type) || isInline(element);
+  editor.isInline = (element) =>
+    ["link", "button"].includes(element.type) || isInline(element)
 
-  editor.insertText = text => {
+  editor.insertText = (text) => {
     if (text && isUrl(text)) {
-      wrapLink(editor, text);
+      wrapLink(editor, text)
     } else {
-      insertText(text);
+      insertText(text)
     }
-  };
+  }
 
-  editor.insertData = data => {
-    const text = data.getData('text/plain');
+  editor.insertData = (data) => {
+    const text = data.getData("text/plain")
 
     if (text && isUrl(text)) {
-      wrapLink(editor, text);
+      wrapLink(editor, text)
     } else {
-      insertData(data);
+      insertData(data)
     }
-  };
+  }
 
-  return editor;
-};
+  return editor
+}
 
 const withImages = (editor: CustomEditor) => {
-  const { insertData, isVoid } = editor;
+  const { insertData, isVoid } = editor
 
-  editor.isVoid = element => {
-    return element.type === 'image' ? true : isVoid(element);
-  };
+  editor.isVoid = (element) => {
+    return element.type === "image" ? true : isVoid(element)
+  }
 
   editor.insertData = (data: any) => {
-    const text = data.getData('text/plain');
-    const { files } = data;
+    const text = data.getData("text/plain")
+    const { files } = data
 
     if (files && files.length > 0) {
       for (const file of files) {
-        const reader = new FileReader();
-        const [mime] = file.type.split('/');
+        const reader = new FileReader()
+        const [mime] = file.type.split("/")
 
-        if (mime === 'image') {
-          reader.addEventListener('load', () => {
-            const url = reader.result;
-            insertImage(editor, url as string);
-          });
+        if (mime === "image") {
+          reader.addEventListener("load", () => {
+            const url = reader.result
+            insertImage(editor, url as string)
+          })
 
-          reader.readAsDataURL(file);
+          reader.readAsDataURL(file)
         }
       }
     } else if (isImageUrl(text)) {
-      insertImage(editor, text);
+      insertImage(editor, text)
     } else {
-      insertData(data);
+      insertData(data)
     }
-  };
+  }
 
-  return editor;
-};
+  return editor
+}
 
-const initialValue = [
+export const INITIAL_EDITOR_VALUE = JSON.stringify([
   {
-    type: 'paragraph',
-    children: [{ text: 'Delete me and explore your way in' }],
+    type: "paragraph",
+    children: [{ text: "Delete me and explore your way in" }],
   },
-];
+])
+
+export const INITIAL_EDITOR_HTML_STRING = `<p>Delete me and explore your way in</p>`
+
+export interface SlateEditorContent {
+  html: string
+  slate: string
+}
+
+export const DEFAULT_EDITOR_CONTENT: SlateEditorContent = {
+  html: INITIAL_EDITOR_HTML_STRING,
+  slate: INITIAL_EDITOR_VALUE,
+}
 
 const SlateEditor: React.FC<{
-  onHtmlChange: (value: any) => void;
-  onContentChange: (value: any) => void;
-}> = ({ onHtmlChange, onContentChange }) => {
-  const renderElement = useCallback((props: any) => <Element {...props} />, []);
-  const renderLeaf = useCallback((props: any) => <Leaf {...props} />, []);
+  onEditorChange: (editorContent: SlateEditorContent) => void
+  content: SlateEditorContent
+  editorId?: string
+  readOnly?: boolean
+}> = ({
+  onEditorChange,
+  content,
+  editorId = generateMongoId(),
+  readOnly = false,
+}) => {
+  const renderElement = useCallback((props: any) => <Element {...props} />, [])
+  const renderLeaf = useCallback((props: any) => <Leaf {...props} />, [])
   const editor: CustomEditor = useMemo(
     () => withImages(withInLines(withHistory(withReact(createEditor())))),
     []
-  );
+  )
 
-  const onKeyDown: React.KeyboardEventHandler = event => {
+  const onKeyDown: React.KeyboardEventHandler = (event) => {
     for (const hotkey in HOTKEYS) {
       if (isHotkey(hotkey, event as any)) {
-        event.preventDefault();
-        const mark = HOTKEYS[hotkey];
-        toggleMark(editor, mark);
+        event.preventDefault()
+        const mark = HOTKEYS[hotkey]
+        toggleMark(editor, mark)
       }
     }
 
-    const { selection } = editor;
+    const { selection } = editor
 
     if (selection && Range.isCollapsed(selection)) {
-      const { nativeEvent } = event;
+      const { nativeEvent } = event
 
-      if (isHotkey('left', nativeEvent)) {
-        event.preventDefault();
-        Transforms.move(editor, { unit: 'offset', reverse: true });
-        return;
+      if (isHotkey("left", nativeEvent)) {
+        event.preventDefault()
+        Transforms.move(editor, { unit: "offset", reverse: true })
+        return
       }
 
-      if (isHotkey('right', nativeEvent)) {
-        event.preventDefault();
-        Transforms.move(editor, { unit: 'offset', reverse: false });
-        return;
+      if (isHotkey("right", nativeEvent)) {
+        event.preventDefault()
+        Transforms.move(editor, { unit: "offset", reverse: false })
+        return
       }
     }
-  };
-  const bgColor = useColorModeValue('white', 'gray.800');
-  const borderColor = useColorModeValue('gray.200', 'gray.700');
+  }
+  const bgColor = useColorModeValue("white", "gray.800")
+  const borderColor = useColorModeValue("gray.200", "gray.700")
+
   return (
     <Stack
-      w={'100%'}
-      minW={'480px'}
+      w={"100%"}
+      minW={"480px"}
       spacing={0}
-      boxShadow={'sm'}
-      borderRadius={'4px'}
+      boxShadow={"sm"}
+      border={"1px"}
+      borderColor={borderColor}
+      borderRadius={"4px"}
       onBlur={() => {
-        onHtmlChange(serialize(editor));
-        onContentChange(editor.children);
+        onEditorChange({
+          html: String(serialize(editor)),
+          slate: JSON.stringify(editor.children),
+        })
       }}
     >
       <Slate
         editor={editor}
         //@ts-ignore
-        value={initialValue}
+        value={JSON.parse(content.slate)}
       >
-        <ToolBar />
+        {!readOnly && <ToolBar />}
         <Box
           p={3}
           bg={bgColor}
-          border={'1px'}
+          border={"1px"}
           borderColor={borderColor}
           minH={40}
-          borderBottomRadius={'4px'}
+          borderBottomRadius={"4px"}
         >
           <Editable
+            readOnly={readOnly}
+            id={editorId}
             renderElement={renderElement}
             renderLeaf={renderLeaf}
-            placeholder="Enter some rich text…"
+            placeholder='Enter some rich text…'
             spellCheck
             autoFocus
             onKeyDown={onKeyDown}
@@ -171,7 +208,7 @@ const SlateEditor: React.FC<{
         </Box>
       </Slate>
     </Stack>
-  );
-};
+  )
+}
 
-export default SlateEditor;
+export default SlateEditor
