3e72f137684da29d954211b03d90fea82137c939
Mshindi Creations <114446531+MshindiCreationsInc@users.noreply.github.com>
2022-10-07 19:35:36 +0300

    feat: :zap: Add align to elements

--- Serialize.tsx
+++ Serialize.tsx
@@ -1,46 +1,75 @@
-import { Text } from 'slate';
-import escapeHtml from 'escape-html';
+import { Text } from "slate"
+import escapeHtml from "escape-html"
 
 const serialize = (node: any) => {
   if (Text.isText(node)) {
-    let string = escapeHtml(node.text);
+    let string = escapeHtml(node.text)
 
     //@ts-ignore
     if (node.bold) {
-      string = `<strong>${string}</strong>`;
+      string = `<strong>${string}</strong>`
     }
 
-    return string;
+    //@ts-ignore
+    if (node.italic) {
+      string = `<em>${string}</em>`
+    }
+
+    //@ts-ignore
+    if (node.underline) {
+      string = `<u>${string}</u>`
+    }
+
+    //@ts-ignore
+    if (node.code) {
+      string = `<code>${string}</code>`
+    }
+
+    return string
   }
 
-  const children = node.children.map((n: any) => serialize(n)).join('');
+  const children = node.children.map((n: any) => serialize(n)).join("")
+
+  const align = node.align ? `align="${node.align}"` : ""
 
   switch (node.type) {
-    case 'paragraph':
-      return `<p>${children}</p>`;
-    case 'block-quote':
-      return `<blockquote>${children}</blockquote>`;
-    case 'bulleted-list':
-      return `<ul>${children}</ul>`;
-    case 'heading-one':
-      return `<h1>${children}</h1>`;
-    case 'heading-two':
-      return `<h2>${children}</h2>`;
-    case 'heading-three':
-      return `<h3>${children}</h3>`;
-    case 'list-item':
-      return `<li>${children}</li>`;
-    case 'numbered-list':
-      return `<ol>${children}</ol>`;
-    case 'link':
-      return `<a href="${escapeHtml(node.url)}">${children}</a>`;
-    case 'image':
-      return `<img src="${escapeHtml(node.url)}" alt="${escapeHtml(
-        node.alt
-      )}" />`;
+    case "paragraph":
+      return `<p ${align}>${children}</p>`
+    case "block-quote":
+      return `<blockquote ${align}>${children}</blockquote>`
+    case "bulleted-list":
+      return `<ul ${align}>${children}</ul>`
+    case "heading-one":
+      return `<h1 ${align}>${children}</h1>`
+    case "heading-two":
+      return `<h2 ${align}>${children}</h2>`
+    case "heading-three":
+      return `<h3 ${align}>${children}</h3>`
+    case "list-item":
+      return `<li ${align}>${children}</li>`
+    case "numbered-list":
+      return `<ol ${align}>${children}</ol>`
+    case "link":
+      return `<a ${align} target="_blank"   href="${escapeHtml(
+        node.url
+      )}">${children}</a>`
+    case "image":
+      if (node.align) {
+        return `<div style="text-align: ${node.align}; padding: 4px">
+        <img 
+        style="max-height:20em; max-width:90%; border-radius: 4px; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;"
+        src="${escapeHtml(node.url)}" alt="${escapeHtml(
+          node.alt || "image"
+        )}" />
+        </div>`
+      } else {
+        return `<img style="max-height:20em; max-width:90%; border-radius: 4px; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;" src="${escapeHtml(
+          node.url
+        )}" alt="${escapeHtml(node.alt || "image")}" />`
+      }
     default:
-      return children;
+      return children
   }
-};
+}
 
-export default serialize;
+export default serialize
